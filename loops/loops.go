fe1c38f28274bd2f6db1fd6f5ab592574812ae95
Marcos Lisboa <programador09@speedgov.com.br>
2023-05-23 23:20:31 -0300

    feat: loops

---
+++ loops/loops.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	i := 0
+
+	// primeira forma de escrever um for
+	for i < 10 {
+		i++
+		fmt.Println(i)
+		time.Sleep(time.Second)
+	}
+
+	fmt.Println(i)
+
+	// segunda forma de fazer um for
+	for j := 0; j < 10; j++ {
+		fmt.Printf("Incrementando j: %v\n", j)
+		time.Sleep(time.Second)
+	}
+
+	nomes := [3]string{"João", "Davi", "Lucas"}
+	
+	//  posição, elemento
+	for indice, nome := range nomes {
+		fmt.Printf("%v: %v\n", indice, nome)
+	}
+}
