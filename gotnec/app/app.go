d49face73ebc2a6509bab34fe3d270d2ce7f2ddc
Marcos Lisboa <lisboamab@gmail.com>
2023-06-04 15:43:56 +0000

    feat: finish my first CLI

--- gotnec/app/app.go
+++ gotnec/app/app.go
@@ -1,6 +1,10 @@
 package app
 
 import (
+	"fmt"
+	"log"
+	"net"
+
 	"github.com/urfave/cli"
 )
 
@@ -8,5 +12,54 @@ func Gerar() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicação de Linha de Comando"
 	app.Usage = "Busca IPs e servidores"
+
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com.br",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:   "ip",
+			Usage:  "Busca IPS de endereços na internet",
+			Flags:  flags,
+			Action: buscarIps,
+		},
+		{
+			Name:   "servers",
+			Usage:  "Busca o nome dos servidores",
+			Flags:  flags,
+			Action: buscaServidores,
+		},
+	}
+
 	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+	ips, erro := net.LookupIP(host)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+}
+
+func buscaServidores(c *cli.Context) {
+	host := c.String("host")
+
+	servers, erro := net.LookupNS(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
+	}
 }
